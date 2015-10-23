f8f052326c07a678a451df9ef19294f06b3a617e
aetex_000 <aete.xd@hotmail.com>
2015-10-23 16:07:32 -0300

    adsfrgwsdfsa

--- protected/views/site/informacion.php
+++ protected/views/site/informacion.php
@@ -1,17 +1,20 @@
 <div class="container">
-    <div class="row">
-        <h1>Bootstrap 3 Lightbox image gallery using Modal</h1>
+    <div class="row half left col-md-6">
+        <h1>Imagenes</h1>
 
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p4.jpg"></a></div>
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 2" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p2.jpg"></a></div>
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 3" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p3.jpg"></a></div>
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 4" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p2.jpg"></a></div>
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p4.jpg"></a></div>
-        <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p3.jpg"></a></div>
-
-        <hr>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail " src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p4.jpg"></a></div>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p2.jpg"></a></div>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p3.jpg"></a></div>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p2.jpg"></a></div>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p4.jpg"></a></div>
+        <div class="col-lg-3 col-sm-4 col-xs-6"><a  href="#"><img class="thumbnail img-responsive" src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p3.jpg"></a></div>
 
         <hr>
+
+        <hr>
+    </div>
+    <div class="row half right col-md-6">
+        <img class="thumbnail " src="<?php echo Yii::app()->request->baseUrl; ?>/images/propiedades/p4.jpg"></a>
     </div>
 </div>
 <div tabindex="-1" class="modal fade" id="myModal" role="dialog">
@@ -19,7 +22,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button class="close" type="button" data-dismiss="modal">&times;</button>
-                <h3 class="modal-title">Heading</h3>
             </div>
             <div class="modal-body">
 
