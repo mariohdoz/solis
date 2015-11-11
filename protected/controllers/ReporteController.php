6d2097cf8d169140de3fd03a8ac366f08106425d
aetex_000 <aete.xd@hotmail.com>
2015-11-11 20:03:17 -0300

    reportes

--- protected/controllers/ReporteController.php
+++ protected/controllers/ReporteController.php
@@ -15,19 +15,31 @@ Class ReporteController extends Controller
     }
     public function actionGenerarPdf()
     {
+
         $model =Propiedad::model()->findAll(); //Consulta para buscar todos los registros
-        $mPDF1 = Yii::app()->ePdf->mpdf('utf-8','A4-L','','',15,15,35,25,9,9,'L'); //Esto lo pueden configurar como quieren, para eso deben de entrar en la web de MPDF para ver todo lo que permite.
+        $mPDF1 = Yii::app()->ePdf->mpdf('utf8_encode()','A4-L','','',15,15,35,25,9,9,'L'); //Esto lo pueden configurar como quieren, para eso deben de entrar en la web de MPDF para ver todo lo que permite.
         $mPDF1->useOnlyCoreFonts = true;
         $mPDF1->SetTitle("Cotización - Reporte");
         $mPDF1->SetAuthor("Propiedades Sol y Cobre");
         $mPDF1->SetWatermarkText("");
-        $mPDF1->showWatermarkText = false;
+        $mPDF1->showWatermarkText = true;
         $mPDF1->watermark_font = 'DejaVuSansCondensed';
         $mPDF1->watermarkTextAlpha = 0.1;
-
+        $mPDF1->ignore_invalid_utf8 = true;
         $mPDF1->SetDisplayMode('fullpage');
         $mPDF1->WriteHTML($this->renderPartial('cotizarpdf', array('model'=>$model), true)); //hacemos un render partial a una vista preparada, en este caso es la vista pdfReport
         $mPDF1->Output('Cotización Propiedad'.date('YmdHis'),'I');  //Nombre del pdf y parámetro para ver pdf o descargarlo directamente.
         exit;
+    }
+    public function actionAdmin()
+    {
+        $model=new Propiedad('search');
+        $model->unsetAttributes();  // clear any default values
+        if(isset($_GET['Propiedad']))
+            $model->attributes=$_GET['Propiedad'];
+
+        $this->render('admin',array(
+            'model'=>$model,
+        ));
     }
 }
--- protected/views/reporte/cotizarpdf.php
+++ protected/views/reporte/cotizarpdf.php
@@ -2,8 +2,17 @@
 
 <html>
 <head>
+  <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css"> <!-- Resource style -->
 </head>
 <body>
+<?php $form=$this->beginWidget('CActiveForm', array(
+    'id'=>'admin-form',
+  // Please note: When you enable ajax validation, make sure the corresponding
+  // controller action is handling ajax validation correctly.
+  // There is a call to performAjaxValidation() commented in generated controller code.
+  // See class documentation of CActiveForm for details on this.
+    'enableAjaxValidation'=>false,
+)); ?>
 <style>
 
 
@@ -26,7 +35,70 @@
 
   <sethtmlpageheader name="myheader" value="on" show-this-page="1" />
   <sethtmlpagefooter name="myfooter" value="on" />
+<h1>Cotización</h1>
+<div class="tabla" >
+  <table >
+    <tr>
+      <td>
+        ÍTEM
+      </td>
+      <td >
+        ARRIENDO DE PROPIEDAD
+      </td>
+      <td>
+        P. UNITARIO
+      </td>
+      <td>
+        TOTAL
+      </td>
+    </tr>
+    <tr>
+      <td >
+        Row 1
+      </td>
+      <td>
+        Row 1
+      </td>
+      <td>
+        Row 1
+      </td>
+      <td>
+        Row 1
+      </td>
+    </tr>
+    <tr>
+      <td >
+        Row 2
+      </td>
+      <td>
+        Row 2
+      </td>
+      <td>
+        Row 2
+      </td>
+      <td>
+        Row 2
+      </td>
+    </tr>
+    <tr>
+      <td >
+        Row 3
+      </td>
+      <td>
+        Row 3
+      </td>
+      <td>
+        Row 3
+      </td>
+      <td>
+        Row 3
+      </td>
+    </tr>
+  </table>
+</div>
 
-  <div style="text-align: right">Lo que sea que se quiera poner</div>
+<div style="text-align: center"><?php echo $data->servicio_propiedad; ?></div>
+
+<?php $this->endWidget(); ?>
 </body>
 </html>
