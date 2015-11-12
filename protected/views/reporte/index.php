693c0b4e0c00d840561dd553f9874370feb6adfd
aetex_000 <aete.xd@hotmail.com>
2015-11-12 00:58:28 -0300

    reportes asdas

--- protected/views/reporte/index.php
+++ protected/views/reporte/index.php
@@ -13,7 +13,7 @@
     <section class="content">
         <?php $form=$this->beginWidget('CActiveForm', array(
             'id'=>'admin-form',
-            // Please note: When you enable ajax validation, make sure the corresponding
+            // Pleas e note: When you enable ajax validation, make sure the corresponding
             // controller action is handling ajax validation correctly.
             // There is a call to performAjaxValidation() commented in generated controller code.
             // See class documentation of CActiveForm for details on this.
